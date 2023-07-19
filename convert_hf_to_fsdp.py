0efa638119afd06c10c2f00a8e5f3c2ed030f488
Khalid Saifullah <khalid277144@gmail.com>
2023-07-19 11:37:36 -0700

    auth token in convert hf to fsdp

--- convert_hf_to_fsdp.py
+++ convert_hf_to_fsdp.py
@@ -11,9 +11,12 @@ if __name__ == "__main__":
     parser.add_argument("--save_path_hf", type=str, default=None, help="This is the path to save the model in HF format, is optional")
     parser.add_argument("--add_tokens", type=int, default=1, help="Number of additional tokens to add to the model")
     parser.add_argument("--cache_dir", type=str, default=None, help="This can be used to store the HF model in a different location than the default if using hf path as opposed to local directory")
+    parser.add_argument("--auth_token", type=str, default=None, help="To access private models on huggingface, you need to provide an auth token")
     args = parser.parse_args()
-
-    model = transformers.AutoModelForCausalLM.from_pretrained(args.load_path, cache_dir=args.cache_dir, trust_remote_code=True)
+    if args.auth_token is not None:
+        model = transformers.AutoModelForCausalLM.from_pretrained(args.load_path, cache_dir=args.cache_dir, trust_remote_code=True, use_auth_token=args.auth_token)
+    else:
+        model = transformers.AutoModelForCausalLM.from_pretrained(args.load_path, cache_dir=args.cache_dir, trust_remote_code=True)
     model = model.to(model.config.torch_dtype) # from_pretrained does not load model weights to the default type, so we have to do it manually
     if args.add_tokens > 0:
         model.resize_token_embeddings(model.config.vocab_size + args.add_tokens)
@@ -29,7 +32,10 @@ if __name__ == "__main__":
     # this is only necessary weight load_path is pointing to a huggingface model
     if args.save_path_hf is not None:
         model.save_pretrained(args.save_path_hf)
-        transformers.AutoTokenizer.from_pretrained(args.load_path, cache_dir=args.cache_dir).save_pretrained(args.save_path_hf)
+        if args.auth_token is not None:
+            transformers.AutoTokenizer.from_pretrained(args.load_path, cache_dir=args.cache_dir, use_auth_token=args.auth_token).save_pretrained(args.save_path_hf)
+        else:
+            transformers.AutoTokenizer.from_pretrained(args.load_path, cache_dir=args.cache_dir).save_pretrained(args.save_path_hf)
         
     # by making nonpersistent buffer persistent, state_dict now includes the original nonpersistent buffer values,
     # which can be used to override the incorrect nonpersistent buffer when initializing the model directly on gpu
