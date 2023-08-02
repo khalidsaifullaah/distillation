57946a50a70049b1a49e1198046e03568b7fc2b0
Khalid Saifullah <khalid277144@gmail.com>
2023-08-02 14:44:38 -0700

    updated acquisiton script code for AL

--- acquisition_AL.py
+++ acquisition_AL.py
@@ -4,59 +4,121 @@ from functools import partial
 import json
 import pickle
 import random
+from typing import Dict
 
 from datasets import Dataset
 import datasets
 import transformers
+from transformers import StoppingCriteria, StoppingCriteriaList
 from transformers.models.llama.configuration_llama import LlamaConfig
 import torch
+from tqdm import tqdm
+import copy
 
 from io_utils import load_jsonlines
 from utils import load_fsdp_ckpt_with_accelerate
 from conversation import get_conv_template
 
+IGNORE_INDEX = -100
+DEFAULT_PAD_TOKEN = "[PAD]"
+DEFAULT_EOS_TOKEN = "</s>"
+DEFAULT_BOS_TOKEN = "<s>"
+DEFAULT_UNK_TOKEN = "<unk>"
+PROMPT_DICT = {
+    "prompt_input": (
+        "Below is an instruction that describes a task, paired with an input that provides further context. "
+        "Write a response that appropriately completes the request.\n\n"
+        "### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:"
+    ),
+    "prompt_no_input": (
+        "Below is an instruction that describes a task. "
+        "Write a response that appropriately completes the request.\n\n"
+        "### Instruction:\n{instruction}\n\n### Response:"
+    ),
+}
 
-def add_padding_token(tokenizer):
-    print("attempt to add padding token if no padding token exists")
-    print("Special tokens before adding padding token: ", tokenizer.special_tokens_map)
-    if not tokenizer.pad_token:
-        tokenizer.add_special_tokens({"pad_token": "[PAD]"})
-    print("Special tokens after adding padding token: ", tokenizer.special_tokens_map)
-    return tokenizer
+class StopOnTokens(StoppingCriteria):
+    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
+        stop_ids = [2]
+        for stop_id in stop_ids:
+            if input_ids[0][-1] == stop_id:
+                return True
+        return False
 
-def apply_conv_template(example, template_type):
+def smart_tokenizer_and_embedding_resize(
+    special_tokens_dict: Dict,
+    tokenizer: transformers.PreTrainedTokenizer,
+    model: transformers.PreTrainedModel,
+):
+    """Resize tokenizer and embedding.
+
+    Note: This is the unoptimized version that may make your embedding size not be divisible by 64.
+    """
+    num_new_tokens = tokenizer.add_special_tokens(special_tokens_dict)
+    model.resize_token_embeddings(len(tokenizer))
+
+    if num_new_tokens > 0:
+        input_embeddings = model.get_input_embeddings().weight.data
+        output_embeddings = model.get_output_embeddings().weight.data
+
+        input_embeddings_avg = input_embeddings[:-num_new_tokens].mean(dim=0, keepdim=True)
+        output_embeddings_avg = output_embeddings[:-num_new_tokens].mean(dim=0, keepdim=True)
+
+        input_embeddings[-num_new_tokens:] = input_embeddings_avg
+        output_embeddings[-num_new_tokens:] = output_embeddings_avg
+
+def apply_conv_template(example):
     # preprocess instructions into prompted inputs
-    conv = get_conv_template(template_type)
-    conv.append_message(conv.roles[0], example['instruction'])
-    conv.append_message(conv.roles[1], "")
-    prompt = conv.get_prompt()
+    prompt_input, prompt_no_input = PROMPT_DICT["prompt_input"], PROMPT_DICT["prompt_no_input"]
+    source  = prompt_input.format_map(example) if example.get("input", "") != "" else prompt_no_input.format_map(example)
 
     example.update({
-        "prompt": prompt
+        "prompt": source
     })
 
     return example
 
-def compute_perplexity_batched(example, model, tokenizer):
+def get_clm_loss(labels, lm_logits):
+    # move labels to correct device to enable model parallelism
+    labels = labels.to(lm_logits.device)
+    # Shift so that tokens < n predict n
+    shift_logits = lm_logits[..., :-1, :].contiguous()
+    shift_labels = labels[..., 1:].contiguous()
+    # Flatten the tokens
+    loss_fct = torch.nn.CrossEntropyLoss(reduction="none")
+    loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
+    loss_per_example = loss.view(shift_logits.size(0), shift_logits.size(1)).mean(axis=1)
+    return loss_per_example
+
+def compute_perplexity_batched(example, model, tokenizer, kwargs):
     prompt = example['prompt']
-    print(prompt)
     encoding = tokenizer(prompt, 
                           return_tensors="pt",
                           padding="longest",
                           max_length=tokenizer.model_max_length,
                           truncation=True,
                       )
-    encoding = encoding.to(model.device)
+    encoding.pop('token_type_ids', None)
+    encoding = {k: v.cuda() for k,v in encoding.items()}
+    # labels = copy.deepcopy(encoding['input_ids'])
+    # labels_lens = torch.sum(labels != tokenizer.pad_token_id, dim=-1).tolist()
+    # labels[labels == tokenizer.pad_token_id] = IGNORE_INDEX
     with torch.no_grad():
-        model_output = model(**encoding)
-        from IPython import embed; embed()
-        model_output = model_output[:, input_len:].cpu()
-        ppl = 0
+        model_output = model.generate(**encoding, **kwargs)
+        logits = torch.nan_to_num(torch.stack(model_output.scores).transpose(0,1))
+        labels = model_output.sequences[:, encoding['input_ids'].shape[-1]:]
+        loss = get_clm_loss(labels, logits.float())
+        ppl = torch.exp(loss).tolist()
+        # input_len = encoding['input_ids'].shape[-1]
+        # output_sequences = model_output.sequences[:, input_len:].cpu()
+        # decoded_output = tokenizer.batch_decode(output_sequences, skip_special_tokens=True, clean_up_tokenization_spaces=False)
 
     del example['prompt']
-    example.update({"ppl": ppl}) 
+    example.update({"ppl": ppl})
+    # example.update({"decoded_output": decoded_output})
 
     return example
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -65,26 +127,41 @@ if __name__ == "__main__":
     parser.add_argument("--model_config_path", default="/home/ksaifullah/llama2_7B_hf", type=str)
     parser.add_argument("--template_type", default="alpaca", type=str)
     parser.add_argument("--file_path", default="datasets/dolphin.jsonl", type=str)
-    parser.add_argument("--save_file_name", default="outputs/answers/self-instruct_llama7B.jsonl", type=str)
-    parser.add_argument("--batch_size", default=1, type=int)
-    parser.add_argument("--data_fraction", default=0.05, type=float)
+    parser.add_argument("--save_file_name", default="outputs/pool_dataset.jsonl", type=str)
+    parser.add_argument("--batch_size", default=4, type=int)
+    parser.add_argument("--data_fraction", default=0.001, type=float)
     parser.add_argument("--seed", default=42, type=int)
     parser.add_argument("--debug", action='store_true', help="This reduce the number of generation examples to 4, so that we can debug faster.")
     args = parser.parse_args()
 
-    model_config = transformers.AutoConfig.from_pretrained(args.model_config_path)
-    if isinstance(model_config, LlamaConfig):
-        model_config.vocab_size += 1 # hardcode the vocab size for llama... 
+    # model_config = transformers.AutoConfig.from_pretrained(args.model_config_path)
+    # if isinstance(model_config, LlamaConfig):
+    #     model_config.vocab_size += 1 # hardcode the vocab size for llama...
 
-    model = load_fsdp_ckpt_with_accelerate(args.model, model_config, hf_dummy_path=args.model_config_path, wrapped_class="LlamaDecoderLayer" if 'llama' in args.model else "OPTDecoderLayer")
+    # model = load_fsdp_ckpt_with_accelerate(args.model, model_config, hf_dummy_path=args.model_config_path, wrapped_class="LlamaDecoderLayer" if 'llama' in args.model else "OPTDecoderLayer")
+    model = transformers.AutoModelForCausalLM.from_pretrained("../llama2_7B_chat_hf/", torch_dtype=torch.float16, trust_remote_code=True).cuda()
     tokenizer = transformers.AutoTokenizer.from_pretrained(
-            args.model_config_path,
+            "../llama2_7B_chat_hf/",
             model_max_length=2048,
             padding_side="left",
-            use_fast=False,
+            use_fast=True,
         )
-    add_padding_token(tokenizer)
-    
+    special_tokens_dict = dict()
+    if tokenizer.pad_token is None:
+        special_tokens_dict["pad_token"] = DEFAULT_PAD_TOKEN
+    if tokenizer.eos_token is None:
+        special_tokens_dict["eos_token"] = DEFAULT_EOS_TOKEN
+    if tokenizer.bos_token is None:
+        special_tokens_dict["bos_token"] = DEFAULT_BOS_TOKEN
+    if tokenizer.unk_token is None:
+        special_tokens_dict["unk_token"] = DEFAULT_UNK_TOKEN
+
+    smart_tokenizer_and_embedding_resize(
+        special_tokens_dict=special_tokens_dict,
+        tokenizer=tokenizer,
+        model=model,
+    )
+
     ## set the models to eval mode
     model = model.eval()
     
@@ -94,10 +171,10 @@ if __name__ == "__main__":
     # randomly sampling 1000 examples from the pool data
     sampled_data = pool_data.shuffle(seed=args.seed).select(range(1000))
     #remove the sampled data from the pool data with .filter()
-    pool_data.set_format('pandas')
-    df = pool_data[:]
-    df = df[~df.index.isin(sampled_data['id'])]
-    pool_data = Dataset.from_pandas(df, preserve_index=False)
+    # pool_data.set_format('pandas')
+    # df = pool_data[:]
+    # df = df[~df.index.isin(sampled_data['id'])]
+    # pool_data = Dataset.from_pandas(df, preserve_index=False)
 
     used_data_count = int(len(pool_data)*args.data_fraction)
     with open('clusters.pkl', 'rb') as f:
@@ -109,51 +186,36 @@ if __name__ == "__main__":
         'input': [],
         'output': []
     }
-    for c in sampled_clusters:
+    sampled_data_ids = set(sampled_data['id'])
+    for c in tqdm(sampled_clusters):
         idx = random.sample(range(len(clusters[c])), 1)[0]
         sample_id = int(clusters[c][idx][0])  # getting the int from numpy.int64
+        while sample_id in sampled_data_ids:  # we want to get a fresh sample from pool that isn't already present in the training set
+            sample_cluster = random.sample(list(clusters.keys()), 1)[0]
+            idx = random.sample(range(len(clusters[sample_cluster])), 1)[0]
+            sample_id = int(clusters[sample_cluster][idx][0])
         filtered_data['instruction'].append(pool_data[sample_id]['instruction'])
         filtered_data['input'].append(pool_data[sample_id]['input'])
         filtered_data['output'].append(pool_data[sample_id]['output'])
 
     pool_data = Dataset.from_dict(filtered_data)
 
-    from IPython import embed; embed()
-
-    # if "dolly" in args.file_path or "vicuna" in args.file_path or "user_oriented_instructions" in args.file_path:
-    #     tasks = load_jsonlines(args.file_path)
-    #     raw_data = Dataset.from_list(tasks)
-    #     if "dolly" in args.file_path:
-    #         question_sources = "dolly"
-    #     elif "vicuna" in args.file_path:
-    #         question_sources = "vicuna"
-    #         raw_data = raw_data.rename_column("text", "instruction")
-    #     elif "user_oriented_instructions" in args.file_path:
-    #         question_sources = "alpaca"
-    # elif "alpaca_eval" in args.file_path:
-    #     raw_data = datasets.load_dataset("tatsu-lab/alpaca_eval", "alpaca_eval")["eval"]
-    #     # set generator field to model name
-    #     raw_data = raw_data.map(lambda x: {"generator": args.model_name if args.model_name else args.model})
-
     ## preprocess
-    eval_preproc = partial(apply_conv_template, template_type=args.template_type)
+    eval_preproc = partial(apply_conv_template)
     pool_data = pool_data.map(eval_preproc)
+    # pool_data = pool_data.map(lambda examples: {"prompt": [examples['instruction'][i]+' '+examples['input'][i] for i in range(len(examples['input']))]}, batched=True)
 
-    ## run generation
-    # generate_kwargs = dict(max_length=1024, do_sample=False, top_p=0.1, 
-    #                        num_return_sequences=1, temperature=0.1, 
-    #                        repetition_penalty=1.2)
+    generate_kwargs = dict(max_new_tokens=256, do_sample=False, top_p=0.9, temperature=0.6,
+                           num_return_sequences=1, output_scores=True, return_dict_in_generate=True, 
+                           stopping_criteria=StoppingCriteriaList([StopOnTokens()]))
     get_ppl = partial(compute_perplexity_batched, 
                        model=model,  
-                       tokenizer=tokenizer)
-
+                       tokenizer=tokenizer,
+                       kwargs=generate_kwargs)
+    from IPython import embed; embed()
     dataset_w_responses = pool_data.map(get_ppl,
                                             batched=True,
-                                            batch_size=args.batch_size)
-    if "alpaca_eval" in args.file_path:
-        # stringify the metadata, so that it becomes hashable
-        dataset_w_responses = dataset_w_responses.map(lambda x: {"metadata": json.dumps(x["metadata"])})
-        dataset_w_responses.to_json(args.save_file_name, orient="records", lines=False, indent=True)
-        
-    else:
-        dataset_w_responses.to_json(args.save_file_name)
+                                            batch_size=args.batch_size,
+                                            num_proc=4)
+    from IPython import embed; embed()
+    # dataset_w_responses.to_json(args.save_file_name)
